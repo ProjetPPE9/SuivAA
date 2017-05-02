cb224b97cc2cae0ffca5797f9edc7e0075575a54
Maxime <maxime.carpentier62350@gmail.com>
2017-05-02 12:11:34 +0200

    Ajout vues

--- app/src/main/java/com/example/matt/suivaa/MainActivity.java
+++ app/src/main/java/com/example/matt/suivaa/MainActivity.java
@@ -1,16 +1,32 @@
 package com.example.matt.suivaa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button boutonCo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        boutonCo = (Button)findViewById(R.id.btnConnexion);
+
+        boutonCo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent passageVue1 = new Intent(MainActivity.this, VisiteActivity.class );
+                startActivity(passageVue1);
+            }
+        });
         //testClasseDAO();
     }
 
@@ -25,5 +41,9 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(MainActivity.this, unCabinetDAO.getCabinet(1).getAdresse().toString(),
                 Toast.LENGTH_LONG).show();
         //test
+
+
     }
+
+
 }
